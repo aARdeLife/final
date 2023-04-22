2f4370e3410f588b7bd18b09351d1f907d3b6602
Super Vision <107816707+aARdeLife@users.noreply.github.com>
2023-04-22 11:31:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+let video;
+let model;
+let canvas;
+let ctx;
+let objects = [];
+let selectedIndex = -1;
+let readButton;
+
+async function setupModel() {
+  model = await cocoSsd.load();
+  console.log('Model loaded');
+}
+
+function setup() {
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+
+  canvas = createCanvas(640, 480);
+  canvas.parent('canvas-container');
+  ctx = canvas.drawingContext;
+
+  readButton = document.getElementById('read-objects');
+  readButton.addEventListener('click', readObjects);
+
+  setupModel();
+}
+
+function draw() {
+  image(video, 0, 0);
+
+  if (model) {
+    model.detect(video).then(predictions => {
+      objects = predictions;
+      drawBoxes();
+    });
+  }
+}
+
+function drawBoxes() {
+  for (let i = 0; i < objects.length; i++) {
+    let box = objects[i].bbox;
+
+    if (i === selectedIndex) {
+      stroke('green');
+    } else {
+      stroke('yellow');
+    }
+    strokeWeight(4);
+    noFill();
+    rect(box[0], box[1], box[2], box[3]);
+  }
+}
+
+function mouseClicked() {
+  selectedIndex = -1;
+
+  for (let i = 0; i < objects.length; i++) {
+    let box = objects[i].bbox;
+    if (mouseX >= box[0] && mouseX <= box[0] + box[2] && mouseY >= box[1] && mouseY <= box[1] + box[3]) {
+      selectedIndex = i;
+      break;
+    }
+  }
+}
+
+function mousePressed() {
+  if (mouseButton === CENTER) {
+    mouseClicked();
+  }
+}
+
+function readObjects() {
+  let objectsNames = objects.map(obj => obj.class);
+  let sentence = objectsNames.join(', ');
+  let utterance = new SpeechSynthesisUtterance(`Detected objects are: ${sentence}`);
+  speechSynthesis.speak(utterance);
+}
+ 
