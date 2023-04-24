e41e1b05b7a77f8064a7ec3f7c5d9faaced15ef3
Super Vision <107816707+aARdeLife@users.noreply.github.com>
2023-04-24 23:55:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -34,28 +34,57 @@ async function setupCamera() {
     });
 }
 
-
 function isPointInRect(x, y, rect) {
-    // Check if a point is inside a rectangle
+    return x >= rect[0] && x <= rect[0] + rect[2] && y >= rect[1] && y <= rect[1] + rect[3];
 }
 
 async function fetchWikipediaSummary(title) {
-    // Fetch a summary of the given title from Wikipedia using the Wikipedia REST API
+    const response = await fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(title)}`);
+    if (response.ok) {
+        const data = await response.json();
+        return data.extract;
+    } else {
+        return 'No summary available';
+    }
 }
 
 canvas.addEventListener('click', async event => {
-    // Check if the click occurred inside the bounding box of any detected object
-    // If so, fetch and display the Wikipedia summary for the object in the summaryBox div element
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    for (const prediction of currentPredictions) {
+        if (isPointInRect(x, y, prediction.bbox)) {
+            const summary = await fetchWikipediaSummary(prediction.class);
+            summaryBox.style.display = 'block';
+            summaryBox.style.left = `${prediction.bbox[0] + prediction.bbox[2]}px`;
+            summaryBox.style.top = `${prediction.bbox[1]}px`;
+            summaryBox.textContent = summary;
+            return;
+        }
+    }
+
+    summaryBox.style.display = 'none';
 });
 
-async function detectObjects() {
-    // Use an object detection model to detect objects in the video stream
-    // Draw the bounding boxes on the canvas and display the object class names
+function drawBoundingBoxes(predictions) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(video, 0, 0, video.width, video.height);
+
+    for (const prediction of predictions) {
+        const [x, y, width, height] = prediction.bbox;
+
+        ctx.strokeStyle = '#00FFFF';
+        ctx.lineWidth = 2;
+        ctx.strokeRect(x, y, width, height);
+
+        ctx.fillStyle = '#00FFFF';
+        ctx.font = '14px sans-serif';
+        ctx.fillText(prediction.class, x, y - 10);
+    }
 }
 
-(async function() {
-    const videoElement = await setupCamera();
-    videoElement.play();
-    detectObjects();
+async function detectObjects() {
+    const model = await cocoSsd.load();
 })();
 
