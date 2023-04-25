11d1c61625659169d3c285be9a67c20194ca779c
Super Vision <107816707+aARdeLife@users.noreply.github.com>
2023-04-25 23:31:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -15,66 +15,106 @@ summaryBox.style.display = 'none';
 document.body.appendChild(summaryBox);
 
 async function setupCamera() {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    video: { width: 640, height: 480 },
-    audio: false
-  });
-  video.srcObject = stream;
-  video.onloadedmetadata = () => {
-    video.width = video.videoWidth;
-    video.height = video.videoHeight;
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-  };
-
-  return new Promise((resolve) => {
-    video.onloadeddata = () => {
-      resolve(video);
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video: { width: 640, height: 480 },
+        audio: false
+    });
+    video.srcObject = stream;
+    video.onloadedmetadata = () => {
+        video.width = video.videoWidth;
+        video.height = video.videoHeight;
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
     };
-  });
+
+    return new Promise((resolve) => {
+        video.onloadeddata = () => {
+            resolve(video);
+        };
+    });
 }
 
-// ... (the rest of the code remains the same) ...
+function isPointInRect(x, y, rect) {
+    return x >= rect[0] && x <= rect[0] + rect[2] && y >= rect[1] && y <= rect[1] + rect[3];
+}
+
+async function fetchWikipediaSummary(title) {
+    const response = await fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(title)}`);
+    if (response.ok) {
+        const data = await response.json();
+        return data.extract;
+    } else {
+        return 'No summary available';
+    }
+}
+
+canvas.addEventListener('click', async event => {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    for (const prediction of currentPredictions) {
+        if (isPointInRect(x, y, prediction.bbox)) {
+            const summary = await fetchWikipediaSummary(prediction.class);
+            summaryBox.style.display = 'block';
+            summaryBox.style.left = `${prediction.bbox[0] + prediction.bbox[2]}px`;
+            summaryBox.style.top = `${prediction.bbox[1]}px`;
+            summaryBox.textContent = summary;
+            return;
+        }
+    }
+
+    summaryBox.style.display = 'none';
+});
+
+function getColorBySize(bbox) {
+    const area = bbox[2] * bbox[3];
+    const maxArea = canvas.width * canvas.height;
+    const ratio = area / maxArea;
+
+    const red = 255;
+    const green = Math.floor(255 * ratio);
+    const blue = 0;
+
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+
+async function drawPredictions(predictions) {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+    ctx.font = '16px sans-serif';
+    ctx.textBaseline = 'top';
+
+    predictions.forEach(prediction => {
+        const x = prediction.bbox[0];
+        const y = prediction.bbox[1];
+        const width = prediction.bbox[2];
+                const height = prediction.bbox[3];
+
+        ctx.strokeStyle = getColorBySize(prediction.bbox);
+        ctx.lineWidth = 2;
+        ctx.strokeRect(x, y, width, height);
+
+        ctx.fillStyle = getColorBySize(prediction.bbox);
+        ctx.fillText(prediction.class, x, y);
+    });
+}
 
 async function detectObjects() {
-  const model = await cocoSsd.load();
+    const model = await cocoSsd.load();
 
-  async function detectFrame() {
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height); // Add this line to draw the video feed onto the canvas
-    const predictions = await model.detect(video);
-    drawPredictions(predictions);
-    requestAnimationFrame(detectFrame);
-  }
+    async function detectFrame() {
+        currentPredictions = await model.detect(video);
+        drawPredictions(currentPredictions);
+        requestAnimationFrame(detectFrame);
+    }
 
-  detectFrame();
+    detectFrame();
 }
 
 (async function() {
-  const videoElement = await setupCamera();
-  videoElement.play();
-  detectObjects();
+    const videoElement = await setupCamera();
+    videoElement.play();
+    detectObjects();
 })();
 
-
-// ... (the rest of the code remains the same) ...
-
-async function detectObjects() {
-  const model = await cocoSsd.load();
-
-  async function detectFrame() {
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height); // Add this line to draw the video feed onto the canvas
-    const predictions = await model.detect(video);
-    drawPredictions(predictions);
-    requestAnimationFrame(detectFrame);
-  }
-
-  detectFrame();
-}
-
-(async function() {
-  const videoElement = await setupCamera();
-  videoElement.play();
-  detectObjects();
-})();
-
-   
