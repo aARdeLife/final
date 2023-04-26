70d7f1cbafc645ae979fb33121f8e0bcd91c308a
Super Vision <107816707+aARdeLife@users.noreply.github.com>
2023-04-26 13:15:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const summaryBox = document.createElement('div');
+
+summaryBox.style.position = 'absolute';
+summaryBox.style.padding = '10px';
+summaryBox.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+summaryBox.style.color = 'white';
+summaryBox.style.borderRadius = '5px';
+summaryBox.style.fontSize = '14px';
+summaryBox.style.maxWidth = '250px';
+summaryBox.style.display = 'none';
+
+document.body.appendChild(summaryBox);
 
 async function setupCamera() {
     const stream = await navigator.mediaDevices.getUserMedia({
@@ -22,6 +34,29 @@ async function setupCamera() {
     });
 }
 
+async function detectObjects() {
+    const model = await cocoSsd.load();
+    async function detectFrame() {
+        const predictions = await model.detect(video);
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.font = '16px sans-serif';
+        ctx.textBaseline = 'top';
+        predictions.forEach(prediction => {
+            const x = prediction.bbox[0];
+            const y = prediction.bbox[1];
+            const width = prediction.bbox[2];
+            const height = prediction.bbox[3];
+            ctx.strokeStyle = getColorBySize(prediction.bbox);
+            ctx.lineWidth = 2;
+            ctx.strokeRect(x, y, width, height);
+            ctx.fillStyle = getColorBySize(prediction.bbox);
+            ctx.fillText(prediction.class, x, y);
+        });
+        requestAnimationFrame(detectFrame);
+    }
+    detectFrame();
+}
+
 function getColorBySize(bbox) {
     const area = bbox[2] * bbox[3];
     const maxArea = canvas.width * canvas.height;
@@ -32,38 +67,6 @@ function getColorBySize(bbox) {
     const blue = 0;
 
     return `rgb(${red}, ${green}, ${blue})`;
-}
-
-async function drawPredictions(predictions) {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.font = '16px sans-serif';
-    ctx.textBaseline = 'top';
-
-    predictions.forEach(prediction => {
-        const x = prediction.bbox[0];
-        const y = prediction.bbox[1];
-        const width = prediction.bbox[2];
-        const height = prediction.bbox[3];
-
-        ctx.strokeStyle = getColorBySize(prediction.bbox);
-        ctx.lineWidth = 2;
-        ctx.strokeRect(x, y, width, height);
-
-        ctx.fillStyle = getColorBySize(prediction.bbox);
-        ctx.fillText(prediction.class, x, y);
-    });
-}
-
-async function detectObjects() {
-    const model = await cocoSsd.load();
-
-    async function detectFrame() {
-        const predictions = await model.detect(video);
-        drawPredictions(predictions);
-        requestAnimationFrame(detectFrame);
-    }
-
-    detectFrame();
 }
 
 (async function() {
