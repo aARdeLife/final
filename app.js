77e109dd8544c40f08198f97a6818dddbd775bdc
Super Vision <107816707+aARdeLife@users.noreply.github.com>
2023-04-26 13:27:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -34,28 +34,38 @@ async function setupCamera() {
     });
 }
 
-async function detectObjects() {
-    const model = await cocoSsd.load();
-    async function detectFrame() {
-        const predictions = await model.detect(video);
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        ctx.font = '16px sans-serif';
-        ctx.textBaseline = 'top';
-        predictions.forEach(prediction => {
-            const x = prediction.bbox[0];
-            const y = prediction.bbox[1];
-            const width = prediction.bbox[2];
-            const height = prediction.bbox[3];
-            ctx.strokeStyle = getColorBySize(prediction.bbox);
-            ctx.lineWidth = 2;
-            ctx.strokeRect(x, y, width, height);
-            ctx.fillStyle = getColorBySize(prediction.bbox);
-            ctx.fillText(prediction.class, x, y);
-        });
-        requestAnimationFrame(detectFrame);
-    }
-    detectFrame();
+function isPointInRect(x, y, rect) {
+    return x >= rect[0] && x <= rect[0] + rect[2] && y >= rect[1] && y <= rect[1] + rect[3];
 }
+
+async function fetchWikipediaSummary(title) {
+    const response = await fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(title)}`);
+    if (response.ok) {
+        const data = await response.json();
+        return data.extract;
+    } else {
+        return 'No summary available';
+    }
+}
+
+canvas.addEventListener('click', async event => {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    for (const prediction of currentPredictions) {
+        if (isPointInRect(x, y, prediction.bbox)) {
+            const summary = await fetchWikipediaSummary(prediction.class);
+            summaryBox.style.display = 'block';
+            summaryBox.style.left = `${prediction.bbox[0] + prediction.bbox[2]}px`;
+            summaryBox.style.top = `${prediction.bbox[1]}px`;
+            summaryBox.textContent = summary;
+            return;
+        }
+    }
+
+    summaryBox.style.display = 'none';
+});
 
 function getColorBySize(bbox) {
     const area = bbox[2] * bbox[3];
@@ -67,6 +77,40 @@ function getColorBySize(bbox) {
     const blue = 0;
 
     return `rgb(${red}, ${green}, ${blue})`;
+}
+
+async function drawPredictions(predictions) {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.font = '16px sans-serif';
+    ctx.textBaseline = 'top';
+
+    predictions.forEach(prediction => {
+        const x = prediction.bbox[0];
+        const y = prediction.bbox[1];
+        const width = prediction.bbox[2];
+        const height = prediction.bbox[3];
+
+        ctx.strokeStyle = getColorBySize(prediction.bbox);
+        ctx.lineWidth = 2;
+        ctx.strokeRect(x, y, width, height);
+
+        ctx.fillStyle = getColorBySize(prediction.bbox);
+        ctx.fillText(prediction.class, x, y);
+    });
+}
+
+let currentPredictions = [];
+
+async function detectObjects() {
+    const model = await cocoSsd.load();
+
+    async function detectFrame() {
+        currentPredictions = await model.detect(video);
+        drawPredictions(currentPredictions);
+        requestAnimationFrame(detectFrame);
+    }
+
+    detectFrame();
 }
 
 (async function() {
